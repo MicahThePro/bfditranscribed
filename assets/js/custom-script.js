50af8dcf572253b2174b4eb3b258166e139650de
smn8600 <scotty.nordlund@gmail.com>
2024-10-28 13:19:27 -0500

    Added shrink video on scroll

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -3,6 +3,23 @@ function getCurrentDate() {
     return currentDate.toDateString();
 }
 
+function getCurrentTime() {
+    const currentTime = new Date();
+    return currentTime.toLocaleTimeString();
+}
+
+window.addEventListener('scroll', function() {
+    var iframe = document.querySelector('.video-iframe');
+    var highlight = this.document.querySelector('.highlight');
+    if (window.scrollY > 100) { // Adjust the scroll value as needed
+        iframe.classList.add('shrink');
+        highlight.classList.add('shrink');
+    } else {
+        iframe.classList.remove('shrink');
+        highlight.classList.remove('shrink');
+    }
+});
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const dateElement = document.getElementById('current-date');
     if (dateElement) {
@@ -14,7 +31,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
-function getCurrentTime() {
-    const currentTime = new Date();
-    return currentTime.toLocaleTimeString();
-}
+
+
+
